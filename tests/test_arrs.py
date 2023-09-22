dcf36c7b02b6fd21554e17ea0f9d455c7171038f
Andrey <yuryev@yandex.ru>
2023-09-22 20:41:55 +0300

    add new cases for 100% coverage my_slice

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,3 +12,5 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
     assert arrs.my_slice([1, 2, 3], -2, -1) == [2]
+    assert arrs.my_slice([], 1, 3) == []
+    assert arrs.my_slice([1, 2, 3], end=2) == [1, 2]
