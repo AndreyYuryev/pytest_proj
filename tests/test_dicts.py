f97a5201c92d5377f56d03d322d3edc2fbaf6b6c
Andrey <yuryev@yandex.ru>
2023-09-22 21:41:11 +0300

    add test case for get_val

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -2,4 +2,9 @@ from utils import dicts
 
 
 def test_get_val():
-    pass
+    data = {"vcs": "mercurial"}
+    assert dicts.get_val(data, 'vcs') == 'mercurial'
+    assert dicts.get_val(data, "vcs", "git") == 'mercurial'
+    data = {}
+    assert dicts.get_val({}, "vcs", "git") == 'git'
+    assert dicts.get_val({}, "vcs", "bazaar") == 'bazaar'
